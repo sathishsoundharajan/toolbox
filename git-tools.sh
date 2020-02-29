218d5045c8cef12933b5ca178608c8ca0f627203
Sathish <sathish.kumar@partner.samsung.com>
2020-03-01 00:29:39 +0530

    Fix for #1

---
+++ git-tools.sh
@@ -0,0 +1,89 @@
+#!/bin/bash -x
+
+## ENV VARS REQUIRED FOR EXECUTION OF GIT UTILITIES
+export GIT_BRANCH_PREFIX=sathish
+
+# check git command is available
+assert_git_installed() {
+  if ! command -v git 2>&1 >/dev/null; then
+    echo "git required for work, please install git!"
+  fi
+}
+
+# check .git folder available ~ git initialized
+assert_git_folder() {
+  if [[ ! -d ".git" ]]; then
+    echo "git is not initilized in this folder, run git init!"
+  fi
+}
+
+# url check
+assert_jira_url() {
+  match=$(echo $1 | awk '{if ($1 ~ /https:\/\/jira.samsungmtv.com\/browse/) {print}}')
+  if [[ -z $match ]]; then
+    echo "jira signature should be https://jira.samsungmtv.com/browse/{ticketId}"
+  fi
+}
+
+# check gawk command is available
+assert_gawk_installed() {
+  if ! command -v gawk 2>&1 >/dev/null; then
+    echo "gawk is required to run the command!"
+  fi
+}
+
+# validate branch name
+assert_branch_name() {
+  
+}
+
+# get ticket id from branch
+get_ticket_id_from_branch() {
+  local ticketId=$(cat .git/HEAD | awk -F '/' '{print $NF}')
+  echo $ticketId
+}
+
+# utility to read the ticket-id from the branch
+# and prefix it as part of the commit message
+jiracommit() {
+  assert_git_installed
+  assert_git_folder
+
+  if [[ -z $1 ]]; then
+    echo "commit message is required, provide what is the fix or feature name!!"
+    return
+  fi
+
+  if [[ ! -z $1 ]]; then
+    ticketId=$(get_ticket_id_from_branch)
+    commitMsg=$ticketId-$1
+    git diff --name-only --cached
+    echo "commit message: $commitMsg"
+    git commit -n -m $commitMsg
+  fi
+}
+
+# utility to part the jira ticket id from the url and create
+# git branch with your preferred prefixer
+# Example: https://jira.samsungmtv.com/browse/COM-31387
+jirawork() {
+  if [[ -z $1 ]]; then
+    echo "Jira ticket url is required!!" 1>&2
+  fi
+
+  if [[ ! -z $1 ]]; then
+    echo "Creating a branch to work for" $1
+    assert_git_installed
+    assert_git_folder
+    assert_jira_url $1
+    ticketId=$(echo $1 | awk '{split($0, url, "/"); print url[5]}')
+
+    if [[ -z $ticketId ]]; then
+      echo "Unable to identify ticket id from" $ticketId
+    fi
+
+    if [[ ! -z $ticketId ]]; then
+      git checkout -b $GIT_BRANCH_PREFIX/$ticketId
+    fi
+  fi
+}
