af3cb13de2ee8348f2775710c579492be51af377
Sathish <sathish.kumar@partner.samsung.com>
2020-03-14 10:16:53 +0530

    Support install script

--- main.sh
+++ main.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
-## Common utils shared by all files
-. $(dirname "$0")/common/log.sh
-. $(dirname "$0")/common/utils.sh
+TOOLBOX=~/.toolbox
 
-## Setup
-. $(dirname "$0")/mac/installs/apps.sh
-. $(dirname "$0")/mac/setup.sh
+if [ -d ${TOOLBOX} ]; then
+	## Common utils shared by all files
+	. ${TOOLBOX}/common/log.sh
+	. ${TOOLBOX}/common/utils.sh
 
-. $(dirname "$0")/nodejs/pkg.sh
-. $(dirname "$0")/git/jira.sh
+	## Setup
+	. ${TOOLBOX}/mac/installs/apps.sh
+	. ${TOOLBOX}/mac/setup.sh
 
-. $(dirname "$0")/alias/git.sh
+	. ${TOOLBOX}/nodejs/pkg.sh
+	. ${TOOLBOX}/git/jira.sh
+
+	. ${TOOLBOX}/alias/git.sh
+fi
+
+if [ ! -d ${TOOLBOX} ]; then
+	## Common utils shared by all files
+	. $(dirname "$0")/common/log.sh
+	. $(dirname "$0")/common/utils.sh
+
+	## Setup
+	. $(dirname "$0")/mac/installs/apps.sh
+	. $(dirname "$0")/mac/setup.sh
+
+	. $(dirname "$0")/nodejs/pkg.sh
+	. $(dirname "$0")/git/jira.sh
+
+	. $(dirname "$0")/alias/git.sh
+fi
