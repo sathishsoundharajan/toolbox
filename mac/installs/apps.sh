a017a93c66a3d468ed3deeefc5aab52d88672d9b
Sathish <sathish.kumar@partner.samsung.com>
2020-03-14 17:06:02 +0530

    Moved to scripts

--- mac/installs/apps.sh
+++ mac/installs/apps.sh
@@ -16,45 +16,64 @@ setup_iterm2_terminal() {
 		error "iTerm2 not found, installing through brew"
 
 		brew cask install iterm2
-
-		if ! command -v zsh 2>&1 >/dev/null; then
-			error "zsh (shell) not found, installing.."
-			brew install zsh
-		fi
-
-		info "installing oh-my-zsh.."
-		rm -rf /Users/sathishkumar/.oh-my-zsh
-		sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" --unattended --skip-chsh
-
-		## install tree
-		if ! command -v tree 2>&1 >/dev/null; then
-			error "tree not found, installing tree.."
-			brew install tree
-		fi
-
-		info "installing powerlevel9k"
-		rm -rf ~/.oh-my-zsh/custom/themes/powerlevel9k
-		git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
-		info "installing antigen ohmyzsh plugin manager"
-		mkdir -p ~/.antigen
-		curl -L git.io/antigen >~/.antigen/antigen.zsh
-
-		info "download the template"
-		mkdir -p ~/.zsh-template/
-		curl -L https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/templates/zshrc.zsh-template >~/.zsh-template/zshrc.zsh-template
-
-		info "moving ghostwheel zsh theme"
-		mkdir -p ~/.zsh-themes
-		cp  ~/.toolbox/mac/templates/ghostwheel.theme ~/.zsh-themes/ghostwheel.theme
-
-		info "applying zshrc template"
-		cat ~/.zsh-template/zshrc.zsh-template ./mac/templates/.zshrc >~/.zshrc
-
-		info "installing fonts"
-		brew tap homebrew/cask-fonts || error "Already insatalled"
-		brew cask install font-hack-nerd-font || error "Already insatalled"
 	fi
+
+	if ! command -v wget 2>&1 >/dev/null; then
+		error "wget not found, installing.."
+		brew install wget
+	fi
+
+	if ! command -v zsh 2>&1 >/dev/null; then
+		error "zsh (shell) not found, installing.."
+		brew install zsh
+	fi
+
+	info "installing oh-my-zsh.."
+	rm -rf ~/.oh-my-zsh
+	wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+	sh install.sh --skip-chsh --unattended
+
+	## install tree
+	if ! command -v tree 2>&1 >/dev/null; then
+		error "tree not found, installing tree.."
+		brew install tree
+	fi
+
+	info "installing powerlevel9k"
+	rm -rf ~/.oh-my-zsh/custom/themes/powerlevel9k
+	git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+	info "installing antigen ohmyzsh plugin manager"
+	mkdir -p ~/.antigen
+	curl -L git.io/antigen >~/.antigen/antigen.zsh
+
+	info "download the template"
+	mkdir -p ~/.zsh-template/
+	curl -L https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/templates/zshrc.zsh-template > ~/.zsh-template/zshrc.zsh-template
+
+	info "moving ghostwheel zsh theme"
+	mkdir -p ~/.zsh-themes
+
+	if [ -d ~/.toolbox ]; then
+		cp ~/.toolbox/mac/templates/ghostwheel.theme ~/.zsh-themes/ghostwheel.theme
+	fi
+
+	if [ ! -d ~/.toolbox ]; then
+		cp ./mac/templates/ghostwheel.theme ~/.zsh-themes/ghostwheel.theme
+	fi
+
+	info "applying zshrc template"
+	if [ -d ~/.toolbox ]; then
+		cat ~/.zsh-template/zshrc.zsh-template ~/.toolbox/mac/templates/.zshrc > ~/.zshrc
+	fi
+
+	if [ ! -d ~/.toolbox ]; then
+		cat ~/.zsh-template/zshrc.zsh-template ./mac/templates/.zshrc >~/.zshrc
+	fi
+
+	info "installing fonts"
+	brew tap homebrew/cask-fonts || error "Already insatalled"
+	brew cask install font-hack-nerd-font || error "Already insatalled"
 
 }
 
